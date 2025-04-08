55df49ca6bd087b43e0d72a1e122f252d2581932
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-08 10:54:22 -0400

    init

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
 
-export default function App() {
+import { Text, TextInput, View } from "react-native";
+import styles from "./styles";
+
+function Input(props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.textInputContainer}>
+      <Text style={styles.textInputLabel}>{props.label}</Text>
+      <TextInput style={styles.textInput} {...props} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+export default function CollectingTextInput() {
+  const [changedText, setChangedText] = useState("");
+  const [submittedText, setSubmittedText] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Input label="Basic Text Input:" />
+      <Input label="Password Input:" secureTextEntry />
+      <Input label="Return Key:" returnKeyType="search" />
+      <Input label="Placeholder Text:" placeholder="Search" />
+      <Input
+        label="Input Events:"
+        onChangeText={(e) => {
+          setChangedText(e);
+        }}
+        onSubmitEditing={(e) => {
+          setSubmittedText(e.nativeEvent.text);
+        }}
+        onFocus={() => {
+          setChangedText("");
+          setSubmittedText("");
+        }}
+      />
+      <Text>Changed: {changedText}</Text>
+      <Text>Submitted: {submittedText}</Text>
+    </View>
+  );
+}
