ecedb767458659b39d76050b313ff7fec3e2cb8a
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2025-04-08 11:18:57 -0400

    init

--- App.js
+++ App.js
@@ -1,29 +1,53 @@
-import React, { useState } from "react";
-import { View, Text } from "react-native";
-import { Picker } from "@react-native-picker/picker";
-import styles from "./styles";
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
 
+import DropDownPicker from 'react-native-dropdown-picker';
+import styles from './styles';
 
+function MyDropdown() {
 
+  //created three values for open, value, and items that are used to manage the state of the dropdown
+  //open is used to control the visibility of the dropdown
+  //value is used to store the selected value from the dropdown
+  //items is used to store the list of items to be displayed in the dropdown
+  const [open, setOpen] = useState(false);
 
-export default function App() {
+  const [value, setValue] = useState(null);
 
-  const [selectedValue, setSelectedValue] = useState("");
+  const [items, setItems] = useState([
 
+    { label: 'Item 1', value: 'item1' },
 
+    { label: 'Item 2', value: 'item2' },
+
+  ]);
 
   return (
+
     <View style={styles.container}>
-    <Text>Select an Option for a Car</Text>
-      <Picker
-        selectedValue={selectedValue}
-        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}>
-        <Picker.Item label="Mustang" value="must" />
-        <Picker.Item label="Corvette" value="corv" />
-        <Picker.Item label="Camaro" value="cam" />
-        </Picker>
-      <Text>Selected Value: {selectedValue}</Text>
-      </View>
-   
+
+    <DropDownPicker
+
+      open={open}
+
+      value={value}
+
+      items={items}
+
+      setOpen={setOpen}
+
+      setValue={setValue}
+
+      setItems={setItems}
+
+    />
+    <Text>{value}</Text>
+
+    </View>
+    
+
   );
+
 }
+
+export default MyDropdown;
